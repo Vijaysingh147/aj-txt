f91a340c5f098e44b02d67426d209cc6c3491e77
Vijaysingh147 <161139887+Vijaysingh147@users.noreply.github.com>
2024-09-19 10:16:56 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "29882586"
 api_hash = "a1fb8af0655e315111cd43177bac52dc"
-bot_token = "7196477348:AAGchP4OKgREqzW5-BjShAoMENLd1JmJa90"
+bot_token = "8027486919:AAG8FiGxAuzsNBTfuLA8r9ClsbA5jU1a05U"
